18ff37038f285315e5ab71775bbc63293449bae5
arini01 <rahmadianarin@gmail.com>
2021-10-19 12:20:57 +0700

    logo
    Menghapus bagian logo

--- pagesadmin/profileadmin.php
+++ pagesadmin/profileadmin.php
@@ -41,8 +41,8 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="">
-        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Sistem Administrasi</span>
+        <!-- <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo"> -->
+        <span class="ms-4 font-weight-bold">Sistem Administrasi</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
