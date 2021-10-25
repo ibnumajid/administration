2a2aafb84b6921868802f0e118dec759847123ee
Ibnu Majid <ibnum1976@gmail.com>
2021-10-25 09:46:56 +0700

    Update permohonandosen.php

--- pages_dosen/permohonandosen.php
+++ pages_dosen/permohonandosen.php
@@ -339,7 +339,7 @@
                         <option selected>Pilih Jenis Surat</option>
                         <option value="Surat perpindahan">Surat Perpindahan Barang Lab</option>
                         <option value="Surat tugas">Permohonan Surat Tugas</option>
-                        <option value="Surat Cuti">Permohonan Surat Cuti</option>
+                        <option value="Surat SK">Permohonan SK</option>
                     </select>
                 </div>
               </div>
