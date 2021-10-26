21a06207e0a3d233a861156835e64286484ab473
GvnP <gavin.pakpahan@gmail.cpm>
2021-10-26 12:37:39 +0700

    Membetulkan login pada sim dteo

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php 
 include('_database/config.php');
  
@@ -5,7 +6,7 @@ $user = $_POST['user'];
 $pass = $_POST['pass'];
 
 $tambah = mysqli_query($koneksi, "SELECT * FROM masuk WHERE user='$user' and pass='$pass'");
-
+$status = mysqli_query($koneksi, "SELECT * from masuk WHERE status_login='$login");
 $cek = mysqli_num_rows($tambah);
 
 if($cek > 0){
@@ -15,28 +16,31 @@ if($cek > 0){
 
    if($data['status'] == '1')
     {
+    
         session_start();
         $row = mysqli_fetch_array($tambah);
         $_SESSION['user'] ="admin";
         $_SESSION['NIP'] ="0000";
-        header("location:./pagesadmin/billingadmin.php");
+        header("location:pagesadmin/billingadmin.php");
     }
     elseif ($data['status'] == '2')
     {
         session_start();
-        $tambah2 = mysqli_query($koneksi, "SELECT * FROM dosen WHERE dsn_nip='$user' ");
+        $tambah2 = mysqli_query($koneksi, "SELECT * FROM data_dosenb WHERE id_npp='$user' ");
         $data2 = mysqli_fetch_assoc($tambah2);{
-            $_SESSION['user'] =$data2['dsn_nama'];
-            $_SESSION['NIP'] =$data2['dsn_nip'];}    
+            $_SESSION['user']   =$data2['nama_anggota'];
+            $_SESSION['NIP']    =$data2['id_npp'];
         header("location:dosen_2.php");
+        }
+        $login++;
     }
     elseif ($data['status'] == '3')
     {
         session_start();
-        $tambah2 = mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE mhs_nrp='$user' ");
+        $tambah2 = mysqli_query($koneksi, "SELECT * FROM data_mhs WHERE nrp_mhs='$user' ");
         $data2 = mysqli_fetch_assoc($tambah2);{
-            $_SESSION['user'] =$data2['mhs_nama'];
-            $_SESSION['NIP'] =$data2['mhs_nrp'];} 
+            $_SESSION['user'] =$data2['nama_mhs'];
+            $_SESSION['NIP']  =$data2['nrp_mhs'];} 
         header("location:mahasiswa.php");
     }
     elseif ($data['status'] == '4')
