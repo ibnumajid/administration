0a484431b6750b454cf3c480a15bfd9c9b00756f
Ibnu Majid <ibnum1976@gmail.com>
2021-10-30 14:02:16 +0700

    Update rekapmhs.php

--- pagesmahasiswa/rekapmhs.php
+++ pagesmahasiswa/rekapmhs.php
@@ -164,23 +164,11 @@ if ($_SESSION['user'] == '') {
             <li class="nav-item d-flex align-items-center">
               <a href="profilmhsw.php" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none"><?php echo $_SESSION['user'] ?></span>
+                <span class="d-sm-inline d-none"><?php echo $_SESSION['user']?></span>
               </a>
             </li>
-            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li>
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0">
-                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-              </a>
-            </li>
+            
+            <li class="nav-item px-3 d-flex align-items-center"></li>
             <li class="nav-item d-flex align-items-center">
               <a href="../logout.php" href="javascript:;" class="nav-link text-body p-0">
                 <i class="fas fa-sign-out-alt"></i>
