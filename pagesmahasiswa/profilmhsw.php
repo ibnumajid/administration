5de6415069cae573329a538f2b62b7178d501de4
Ibnu Majid <ibnum1976@gmail.com>
2021-11-02 09:03:49 +0700

    Update profilmhsw.php

--- pagesmahasiswa/profilmhsw.php
+++ pagesmahasiswa/profilmhsw.php
@@ -168,7 +168,11 @@
     <!-- End Navbar -->
     <div class="container-fluid">
       <div class="page-header min-height-300 border-radius-xl mt-4" style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%;">
-        <span class="mask bg-gradient-primary opacity-6"></span>
+        <span class="mask bg-gradient-priwc
+        
+        
+        
+        mary opacity-6"></span>
       </div>
       <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
         <div class="row gx-4">
