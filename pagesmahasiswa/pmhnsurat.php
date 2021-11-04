f62e183adce4c1766718ef03cf61d681cfd9631c
Ibnu Majid <ibnum1976@gmail.com>
2021-11-04 13:41:37 +0700

    Update pmhnsurat.php

--- pagesmahasiswa/pmhnsurat.php
+++ pagesmahasiswa/pmhnsurat.php
@@ -281,7 +281,11 @@ session_start();
                         <option selected>Pilih Jenis Surat</option>
                         <option value="Surat Magang">Surat Magang</option>
                         <option value="Surat Tugas Akhir">Surat Tugas Akhir</option>
+                        <option value="Surat Cuti">Surat PBL (Project Based Learning)</option>
                         <option value="Surat Cuti">Surat Cuti</option>
+                        <option value="Surat Cuti">Surat Mengundurkan Diri</option>
+                        <option value="Surat Cuti">Surat Pengajuan Beasiswa</option>
+                        <option value="Surat Cuti">Surat Pengajuan Keinganan UKT</option>
                     </select>
                 </div>
               </div>
