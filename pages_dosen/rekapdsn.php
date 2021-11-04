34c570f96ecd929e369d4a55935fdbd8263e9c8d
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-04 11:48:53 +0700

    membetulkan tampilan pada alur pengiriman dosen
    membetulkan tampilan rekapdosen dan validasi dosen

--- pages_dosen/rekapdsn.php
+++ pages_dosen/rekapdsn.php
@@ -139,7 +139,7 @@
                 </g> 
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Validasi Surat</span>
+            <span class="nav-link-text ms-1">Validasi Surat Mahasiswa</span>
           </a>
         </li>
 
@@ -294,20 +294,6 @@
                 <i class="fa fa-user me-sm-1"></i>
                 <span class="d-sm-inline d-none"><?php echo $_SESSION['user'] ?></span>
               </a>
-            </li>
-            <!--li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li-->
-            <li class="nav-item px-3 d-flex align-items-center">
-              <!--a href="javascript:;" class="nav-link text-body p-0">
-                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-              </a-->
             </li>
             
             <li class="nav-item d-flex align-items-center">
