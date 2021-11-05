b5221572d156106818e9d222d0a231df25437d04
Ibnu Majid <ibnum1976@gmail.com>
2021-11-05 12:30:22 +0700

    Update validasisurat.php

--- pages_dosen/validasisurat.php
+++ pages_dosen/validasisurat.php
@@ -17,7 +17,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="../assets/images/favicon.png">
   <title>
     Sistem Administrasi DTEO
   </title>
@@ -38,7 +38,6 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="">
-        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold">Sistem Administrasi Dosen</span>
       </a>
     </div>
