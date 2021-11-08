59e19d4652f36e202adb8dee74c447d6cf0552e6
AdnanDennis <92702339+AdnanDennis@users.noreply.github.com>
2021-11-08 15:33:48 +0700

    status surat home
    status surat dibagian home dosen

--- dosen_2.php
+++ dosen_2.php
@@ -380,11 +380,9 @@
                         include "./_database/config.php";
 
                     $query = mysqli_query($koneksi, 'SELECT * FROM kirimadmindsn ');
-                    $no = 0;
-                    $no2 = $no++;
                     while ($data = mysqli_fetch_array($query)){
                       $tujuan = $data['nama_dsn'];
-                      if (strpos($tujuan, $_SESSION['user']) !== false) { $no++ ?>
+                      if (strpos($tujuan, $_SESSION['user']) !== false) {?>
                      
                     <li class="list-group-item border-0 d-flex justify-content-between ps-4 mb-2 border-radius-lg">
                       <div class="d-flex flex-column">
@@ -398,11 +396,6 @@
                       </div>
                     </li>
                       <?php }} ?>
-                      <?php if ($no == 1) { ?>
-
-                      <h6 class = "text-center">BELUM ADA SURAT MASUK</h6>
-                      <?php } ?>
-
                   </ul>
                 </div>
               </div>
@@ -425,7 +418,7 @@
                 $query = mysqli_query($koneksi, 'SELECT * FROM suratdosen ORDER BY id_no DESC');
                 while ($data = mysqli_fetch_array($query)) {
                   if ($data['nama_dsn'] == $_SESSION['user']) {
-                    $no++
+
                 ?>
                     <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
                       <div class="d-flex flex-column">
@@ -571,12 +564,6 @@
                 
                <?php } ?>
 
-               <?php if ($no == 1) { ?>
-              <h6 class = "text-center">BELUM ADA SURAT MASUK</h6>
-
-              <?php } ?>
-
-
               </ul>
              </div>
             </div>
@@ -662,97 +649,50 @@
 <!-- Bagian Status Surat -->
             <div class="col-md-5 mt-4">
               <div class="card h-100 mb-4">
-                <div class="card-header pb-0 px-4">
+              
+                <div class="card-header pb-4 px-4">
                   <div class="row">
                     <div class="col-md-6">
                       <h6 class="mb-0">Status Surat</h6>
                     </div>
                     <div class="col-md-6 d-flex justify-content-end align-items-center">
                       <i class="far fa-calendar-alt me-2"></i>
-                      <small>23 September 2021 </small>
+                      <small><?php  echo date('d F o')?></small>
                     </div>
                   </div>
                 </div>
-                <div class="card-body pt-4 p-3">
-                  <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Newest</h6>
-                  <ul class="list-group">
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-down"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Informasi Pelaksanaan Proyek Akhir</h6>
-                          <span class="text-xs">23 September 2021, 12:30 PM</span>
-                        </div>
+                <ul class="list-group">
+                  <?php
+                  include "./_database/config.php";
+                            
+                  $query = mysqli_query($koneksi, 'SELECT * FROM suratdosen ORDER BY id_no DESC ');
+                  while ($data = mysqli_fetch_array($query)){
+
+                    if ($data['nama_dsn'] == $_SESSION['user']) {
+                    ?>
+                      <li class="list-group-item border-0 d-flex justify-content-between px-4 mb-3 ">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-1 text-dark text-sm"><?php echo $data['perihal'] ?></h6>
+                          <span class="text-xs"><?php echo $data['tanggal'] ?></span>
                       </div>
-                      <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                        Surat Masuk
-                      </div>
-                    </li>
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Permohonan Surat Rekomendasi Departemen</h6>
-                          <span class="text-xs">23 September 2021, 11:30 AM</span>
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                        Diajukan
-                      </div>
-                    </li>
+                      <!-- status surat dosen-->
+                      <?php if ($data['status_admin'] == 0) {?>
+                          <div class="d-flex align-items-center text-sm font-weight-bold">
+                            <span class="badge badge-sm bg-gradient-secondary" value="<?php echo $data['status_admin'] ?>">Sedang Di Proses</span>
+                          </div>
+                        <?php } 
+                        else if ($data['status_admin'] == 1) {?>
+                          <div class="d-flex align-items-center text-sm font-weight-bold">
+                            <span class="badge badge-sm bg-gradient-danger" value="<?php echo $data['status_admin'] ?>">Ditolak</span>
+                          </div>
+                        <?php }
+                        else if ($data['status_admin'] == 2) {?>
+                          <div class="d-flex align-items-center text-sm font-weight-bold">
+                            <span class="badge badge-sm bg-gradient-success" value="<?php echo $data['status_admin'] ?>">Disetujui</span>
+                          </div>
+                        <?php } }}?>
+                      </li>
                   </ul>
-                  <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Yesterday</h6>
-                  <ul class="list-group">
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Surat 1</h6>
-                          <span class="text-xs">22 September 2021, 13:45 PM</span>
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                        Disetujui
-                      </div>
-                    </li>
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Surat 2</h6>
-                          <span class="text-xs">22 September 2021, 13:45 PM</span>
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                        Disetujui
-                      </div>
-                    </li>
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Surat 3</h6>
-                          <span class="text-xs">22 September 2021, 13:45 PM</span>
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                        Disetujui
-                      </div>
-                    </li>
-                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                      <div class="d-flex align-items-center">
-                        <button class="btn btn-icon-only btn-rounded btn-outline-dark mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-exclamation"></i></button>
-                        <div class="d-flex flex-column">
-                          <h6 class="mb-1 text-dark text-sm">Surat 4</h6>
-                          <span class="text-xs">22 September 2021, 13:45 PM</span>
-                        </div>
-                      </div>
-                      <div class="d-flex align-items-center text-dark text-sm font-weight-bold">
-                        Pending
-                      </div>
-                    </li>
-                  </ul>
-                </div>
               </div>
             </div>
           </div>
