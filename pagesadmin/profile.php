c302cea60a8631c103938b882e3028b1a777329b
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-09 15:26:29 +0700

    membuat satu pages khusus ganti password

---
+++ pagesadmin/profile.php
@@ -0,0 +1,277 @@
+<?php
+session_start();
+if ($_SESSION['user'] == '') {
+  header("location:home.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <title>
+    Soft UI Dashboard by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="./assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show bg-gray-100">
+  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="">
+        <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">Sistem Administrasi</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+
+<?php if ($_SESSION['status'] == 1) { ?>
+        <li class="nav-item">
+          <a class="nav-link  " href="./pagesadmin/billingadmin.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>credit-card</title>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                    <g transform="translate(1716.000000, 291.000000)">
+                      <g transform="translate(453.000000, 454.000000)">
+                        <path class="color-background opacity-6" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"></path>
+                        <path class="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Home</span>
+          </a>
+        </li>
+<?php } ?>
+
+<?php if ($_SESSION['status'] == 2) { ?>
+        <li class="nav-item">
+          <a class="nav-link  " href="./dosen_2.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>credit-card</title>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                    <g transform="translate(1716.000000, 291.000000)">
+                      <g transform="translate(453.000000, 454.000000)">
+                        <path class="color-background opacity-6" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"></path>
+                        <path class="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Home</span>
+          </a>
+        </li>
+<?php } ?>
+
+<?php if ($_SESSION['status'] == 3) { ?>
+        <li class="nav-item">
+          <a class="nav-link  " href="./mahasiswa.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>credit-card</title>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                    <g transform="translate(1716.000000, 291.000000)">
+                      <g transform="translate(453.000000, 454.000000)">
+                        <path class="color-background opacity-6" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"></path>
+                        <path class="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Home</span>
+          </a>
+        </li>
+<?php } ?>
+  
+        
+        
+        
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  active" href="../pages/profile.html">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>customer-support</title>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                    <g transform="translate(1716.000000, 291.000000)">
+                      <g transform="translate(1.000000, 0.000000)">
+                        <path class="color-background opacity-6" d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z"></path>
+                        <path class="color-background" d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z"></path>
+                        <path class="color-background" d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Ganti Password</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+  </aside>
+  <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg bg-transparent shadow-none position-absolute px-4 w-100 z-index-2">
+      
+    </nav>
+    <!-- End Navbar -->
+    <div class="container-fluid">
+      <div class="page-header min-height-100 border-radius-xl mt-4">
+        <span class="mask bg-gradient-info opacity-6"></span>
+      </div>
+      <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
+        <div class="row gx-4">
+          <div class="col-auto">
+          </div>
+          <div class="col-auto my-auto">
+            <div class="h-100">
+              <h5 class="mb-1">
+                <?php echo $_SESSION['user'] ?>
+              </h5>
+              <p class="mb-0 font-weight-bold text-sm">
+                <?php echo $_SESSION['NIP'] ?>
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12 col-xl-11">
+          <div class="card h-100">
+            <div class="card-header pb-0 p-3">
+              <h6 class="mb-0">Ganti Password <?php echo $_SESSION['pass'] ?></h6>
+            </div>
+            <div class="card-body p-3">
+              <form action = "" method = "post" enctype = "multipart/form-data">
+
+                <div class="form-group row">
+                  <label for="inputPasswordLama" class="col-sm-3 col-form-label">Password Lama</label>
+                  <div class="col-sm-6">
+                    <input name ="pwl" type="password" class="form-control" placeholder="Masukan Password Lama">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="inputPasswordBaru" class="col-sm-3 col-form-label">Password Baru</label>
+                  <div class="col-sm-6">
+                    <input name ="pwb" type="password" class="form-control"  placeholder="Masukan Password Baru">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="KonfirmasiPassword" class="col-sm-3 col-form-label">Konfirmasi Password</label>
+                  <div class="col-sm-6">
+                    <input name ="kpwb" type="password" class="form-control" placeholder="Masukan Ulang Password Baru">
+                  </div>
+                </div>
+
+                <input type="hidden" name="user" value="<?php echo $_SESSION['user'] ?>">
+
+              </div>
+              <div class="d-grid gap-1 col-4 mx-auto" class="btn bg-gradient-info" >
+                  <input class="btn bg-gradient-info" type="submit" name="btnChangePassword" class="btn btn-primary" value="Ganti Password"/>
+              </div>
+
+              </form>  
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <!-- php update surat saat dosen menolak -->
+       <?php
+                include "./_database/config.php";
+                if (isset($_POST['btnChangePassword'])) {
+
+                  $passwordasli = $_SESSION['pass'];
+                  $passwordlama = $_POST['pwl'];
+                  $passwordbaru = $_POST['pwb'];
+                  $konfirmasipassword = $_POST['kpwb'];
+
+                  $user = $_SESSION['NIP'];
+                 
+                  if ($passwordlama == $passwordasli) {
+
+                    if($konfirmasipassword == $passwordbaru) {
+
+                      $query = mysqli_query($koneksi, 'SELECT * FROM masuk' );
+                      $query2 = mysqli_query($koneksi, "UPDATE masuk SET `pass` = '$konfirmasipassword' WHERE user = '$user' ");
+                      session_reset();
+                      if ($query2) {
+                                echo '<a href="./pmhnsurat.php"><script> alert ("Berhasil Merubah Password, Silahkan Logout Untuk Mencoba Password Baru Anda")</script></a>';
+                                ?>  <script> history.pushState({}, "", "")</script> <?php
+                                session_destroy();
+                                        } 
+                      else {
+                                echo '<a href="./pmhnsurat.php"><script> alert ("gagal merubah password")</script></a>';
+                              }
+                            }
+                    else { echo '<a href="./pmhnsurat.php"><script> alert ("Konfirmasi Password Salah")</script></a>';
+                      ?>  <script> history.pushState({}, "", "")</script> <?php }
+                          } 
+                  else { 
+                      echo '<a href="./pmhnsurat.php"><script> alert ("Password Lama Salah")</script></a>';
+                      ?>  <script> history.pushState({}, "", "")</script> <?php }
+                        }
+
+                                ?>
+      
+    </div>
+  </div>
+
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/popper.min.js"></script>
+  <script src="./assets/js/core/bootstrap.min.js"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+</body>
+
+</html>
