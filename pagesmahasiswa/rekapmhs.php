448be1765b27ce2e2117dc348ed98806f2803ec8
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-09 15:25:34 +0700

    Menambahkan ganti password pada navbar di semua user

--- pagesmahasiswa/rekapmhs.php
+++ pagesmahasiswa/rekapmhs.php
@@ -110,12 +110,12 @@ if ($_SESSION['user'] == '') {
 
 
 
-        <!--profil-->
-        <!--li class="nav-item mt-3">
+ <!--profil-->
+<li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="./profilmhsw.php">
+          <a class="nav-link  " href="../profile.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>customer-support</title>
@@ -132,9 +132,9 @@ if ($_SESSION['user'] == '') {
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Profile</span>
+            <span class="nav-link-text ms-1">Ganti Password</span>
           </a>
-        </li-->
+  </li>
 
 
 
