1309129e23ec355092e791764992f35838a7380c
Ibnu Majid <ibnum1976@gmail.com>
2021-11-09 09:18:00 +0700

    Update permohonandosen.php

--- pages_dosen/permohonandosen.php
+++ pages_dosen/permohonandosen.php
@@ -388,7 +388,7 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Batal</button>
-            <button type="submite" name="input" class="btn bg-gradient-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Kirim Permohonan Surat</button>
+            <button type="submite" name="input" class="btn bg-gradient-info" data-bs-toggle="modal" data-bs-target="#exampleModal">Kirim Permohonan Surat</button>
           </div>
           </form>
         </div>
