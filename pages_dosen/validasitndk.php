6e18b9eec0a77f1716cc5332aa91f6a9f6c20dc3
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-12 14:05:14 +0700

    Kadep sekarang bisa melakukan validasi pada file yang dikirimkan oleh tendik

--- pages_dosen/validasitndk.php
+++ pages_dosen/validasitndk.php
@@ -113,7 +113,7 @@
         </li>
         <?php } ?>
 
-        <!--Validasi Surat-->
+        <!--Validasi Surat Mahasiswa-->
         <li class="nav-item">
           <a class="nav-link  " href="../pages_dosen/validasisurat.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -134,11 +134,10 @@
             <span class="nav-link-text ms-1">Validasi Surat Mahasiswa</span>
           </a>
         </li>
-
+         
         <!--Validasi Surat-->
-        <?php if ($_SESSION['user'] == "Imam Arifin, S.T, MT") {?>
         <li class="nav-item">
-          <a class="nav-link  active" href="../pages_dosen/validasidsn.php">
+          <a class="nav-link  " href="../pages_dosen/validasidsn.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg class="text-dark" width="16px" height="16px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> 
                 <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> 
@@ -157,33 +156,30 @@
             <span class="nav-link-text ms-1">Validasi Surat Dosen</span>
           </a>
         </li>
-        <?php } ?>
 
-            <!-- Validasi Surat Dosen -->
-            <?php if ($_SESSION['user'] == "Imam Arifin, S.T, MT") {?>
-          <li class="nav-item">
-            <a class="nav-link  " href="./validasitendik.php">
-              <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                <svg class="text-dark" width="16px" height="16px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> 
-                  <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> 
-                    <g id="Rounded-Icons" transform="translate(-2319.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero"> 
-                      <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)"> 
-                        <g id="box-3d-50" transform="translate(603.000000, 0.000000)"> 
-                          <path class="color-background" d="M22.7597136,19.3090182 L38.8987031,11.2395234 C39.3926816,10.9925342 39.592906,10.3918611 39.3459167,9.89788265 C39.249157,9.70436312 39.0922432,9.5474453 38.8987261,9.45068056 L20.2741875,0.1378125 L20.2741875,0.1378125 C19.905375,-0.04725 19.469625,-0.04725 19.0995,0.1378125 L3.1011696,8.13815822 C2.60720568,8.38517662 2.40701679,8.98586148 2.6540352,9.4798254 C2.75080129,9.67332903 2.90771305,9.83023153 3.10122239,9.9269862 L21.8652864,19.3090182 C22.1468139,19.4497819 22.4781861,19.4497819 22.7597136,19.3090182 Z" id="Path"></path> 
-                          <path class="color-background" d="M23.625,22.429159 L23.625,39.8805372 C23.625,40.4328219 24.0727153,40.8805372 24.625,40.8805372 C24.7802551,40.8805372 24.9333778,40.8443874 25.0722402,40.7749511 L41.2741875,32.673375 L41.2741875,32.673375 C41.719125,32.4515625 42,31.9974375 42,31.5 L42,14.241659 C42,13.6893742 41.5522847,13.241659 41,13.241659 C40.8447549,13.241659 40.6916418,13.2778041 40.5527864,13.3472318 L24.1777864,21.5347318 C23.8390024,21.7041238 23.625,22.0503869 23.625,22.429159 Z" id="Path" opacity="0.7"></path> 
-                          <path class="color-background" d="M20.4472136,21.5347318 L1.4472136,12.0347318 C0.953235098,11.7877425 0.352562058,11.9879669 0.105572809,12.4819454 C0.0361450918,12.6208008 6.47121774e-16,12.7739139 0,12.929159 L0,30.1875 L0,30.1875 C0,30.6849375 0.280875,31.1390625 0.7258125,31.3621875 L19.5528096,40.7750766 C20.0467945,41.0220531 20.6474623,40.8218132 20.8944388,40.3278283 C20.963859,40.1889789 21,40.0358742 21,39.8806379 L21,22.429159 C21,22.0503869 20.7859976,21.7041238 20.4472136,21.5347318 Z" id="Path" opacity="0.7"></path> 
-                        </g> 
+        <!--Validasi Surat-->
+        <?php if ($_SESSION['user'] == "Imam Arifin, S.T, MT") {?>
+        <li class="nav-item">
+          <a class="nav-link  active" href="../pages_dosen/validasitndk.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg class="text-dark" width="16px" height="16px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> 
+                <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> 
+                  <g id="Rounded-Icons" transform="translate(-2319.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero"> 
+                    <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)"> 
+                      <g id="box-3d-50" transform="translate(603.000000, 0.000000)"> 
+                        <path class="color-background" d="M22.7597136,19.3090182 L38.8987031,11.2395234 C39.3926816,10.9925342 39.592906,10.3918611 39.3459167,9.89788265 C39.249157,9.70436312 39.0922432,9.5474453 38.8987261,9.45068056 L20.2741875,0.1378125 L20.2741875,0.1378125 C19.905375,-0.04725 19.469625,-0.04725 19.0995,0.1378125 L3.1011696,8.13815822 C2.60720568,8.38517662 2.40701679,8.98586148 2.6540352,9.4798254 C2.75080129,9.67332903 2.90771305,9.83023153 3.10122239,9.9269862 L21.8652864,19.3090182 C22.1468139,19.4497819 22.4781861,19.4497819 22.7597136,19.3090182 Z" id="Path"></path> 
+                        <path class="color-background" d="M23.625,22.429159 L23.625,39.8805372 C23.625,40.4328219 24.0727153,40.8805372 24.625,40.8805372 C24.7802551,40.8805372 24.9333778,40.8443874 25.0722402,40.7749511 L41.2741875,32.673375 L41.2741875,32.673375 C41.719125,32.4515625 42,31.9974375 42,31.5 L42,14.241659 C42,13.6893742 41.5522847,13.241659 41,13.241659 C40.8447549,13.241659 40.6916418,13.2778041 40.5527864,13.3472318 L24.1777864,21.5347318 C23.8390024,21.7041238 23.625,22.0503869 23.625,22.429159 Z" id="Path" opacity="0.7"></path> 
+                        <path class="color-background" d="M20.4472136,21.5347318 L1.4472136,12.0347318 C0.953235098,11.7877425 0.352562058,11.9879669 0.105572809,12.4819454 C0.0361450918,12.6208008 6.47121774e-16,12.7739139 0,12.929159 L0,30.1875 L0,30.1875 C0,30.6849375 0.280875,31.1390625 0.7258125,31.3621875 L19.5528096,40.7750766 C20.0467945,41.0220531 20.6474623,40.8218132 20.8944388,40.3278283 C20.963859,40.1889789 21,40.0358742 21,39.8806379 L21,22.429159 C21,22.0503869 20.7859976,21.7041238 20.4472136,21.5347318 Z" id="Path" opacity="0.7"></path> 
                       </g> 
                     </g> 
                   </g> 
-                </svg>
-              </div>
-              <span class="nav-link-text ms-1">Validasi Surat Tendik</span>
-            </a>
-          </li>
-
-          <?php } ?>
-
+                </g> 
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Validasi Surat Tendik</span>
+          </a>
+        </li>
+        <?php } ?>
 
        
           
@@ -336,8 +332,8 @@
                   <thead>
                     <tr>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-3">Perihal</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-1">Nama Dosen</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-1">NIP/NPP Dosen</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-1">Nama Tendik</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-1">NIP Tendik</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-5">Tanggal Pengajuan</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Proses Admin</th>
@@ -349,7 +345,7 @@
                   <?php
                   include '../_database/config.php'; //panggil setiap ingin koneksi ke data
                   $nama = $_SESSION['user'];
-                  $query = mysqli_query($koneksi, 'SELECT * FROM suratdosen ORDER BY id_no DESC');
+                  $query = mysqli_query($koneksi, 'SELECT * FROM surattendik ORDER BY id_no DESC');
                   
                   while ($data = mysqli_fetch_array($query)) {
                   $no++
@@ -371,11 +367,11 @@
                       </td>
                       <!-- nama -->
                       <td>
-                      <h6 class="mb-0 text-sm"><?php echo $data['nama_dsn'] ?></h6>
+                      <h6 class="mb-0 text-sm"><?php echo $data['nama_tdk'] ?></h6>
                       </td>
                       <!-- nrp -->
                       <td>
-                      <h6 class="mb-0 text-sm"><?php echo $data['id_npp'] ?></h6>
+                      <h6 class="mb-0 text-sm"><?php echo $data['id_nip'] ?></h6>
                       </td>
                       <!-- tanggal -->
                       <td class="align-middle text-center">
@@ -437,10 +433,10 @@
                               <div class="row">
                                 <div class="mb-3">
                                   <!-- nama dosen -->
-                                  <label for="formFile" class="form-label">Nama Dosen</label>
+                                  <label for="formFile" class="form-label">Nama Tendik</label>
                                   <label name="nm" class="form-control" aria-label="default input example"><?php echo $data['nama_dsn'] ?></label>
                                   <!-- NPP Dosen -->
-                                  <label for="formFile" class="form-label">NIP/NPP Dosen</label>
+                                  <label for="formFile" class="form-label">NIP Tendik</label>
                                   <label name="nrp" class="form-control" aria-label="default input example"><?php echo $data['id_npp'] ?></label>
                                   <!-- progres -->
                                   <label for="formFile" class="form-label">Nama File</label>
@@ -515,14 +511,14 @@
                       $cat = $_POST['catatan'];
                       
                       
-                      $query = mysqli_query($koneksi, "UPDATE suratdosen SET `status_kadep`='$status' WHERE id_no = '$id' ");
-                      $query = mysqli_query($koneksi, "UPDATE suratdosen SET `catatan`='$cat' WHERE id_no = '$id' ");
+                      $query = mysqli_query($koneksi, "UPDATE surattendik SET `status_kadep`='$status' WHERE id_no = '$id' ");
+                      $query = mysqli_query($koneksi, "UPDATE surattendik SET `catatan`='$cat' WHERE id_no = '$id' ");
                       if($query){
-                        echo '<a href="../pages_dosen/validasisurat.php"><script> alert ("Berhasil Merespon")</script></a>';
+                        echo '<script> alert ("Berhasil Merespon")</script></a>';
                         ?> <script> history.pushState({}, "", "") </script> <?php
                       } 
                       else {
-                        echo '<a href="../pages_dosen/validasisurat.php"><script> alert ("Gagal Merespon")</script></a>';
+                        echo '<script> alert ("Gagal Merespon")</script></a>';
                       }
                     } ?>
                   </tbody>
