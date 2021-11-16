4923a47adb5b67e0a6f6dc374346d156a30df1a1
arini01 <rahmadianarin@gmail.com>
2021-11-16 13:16:42 +0700

    mengatur letak
    mengatur letak tulisan sistem administrasi admin agar ditengah

--- pagesadmin/billingadmin.php
+++ pagesadmin/billingadmin.php
@@ -58,14 +58,15 @@
   </head>
 
   <body class="g-sidenav-show  bg-gray-100">
-    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
-      <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="">
-          <!-- <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo"> -->
-          <span class="ms-4 font-weight-bold">Sistem Administrasi Admin</span>
-        </a>
-      </div>
+  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="">
+        
+        <span class="ms-1 font-weight-bold">Sistem Administrasi Admin</span>
+      </a>
+    </div>
+
       <hr class="horizontal dark mt-0">
       <div class="collapse navbar-collapse  w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
