2462734053b632e86203085628ddbcdff14c9b82
arini01 <rahmadianarin@gmail.com>
2021-11-17 10:15:07 +0700

    Update billingadmin.php

--- pagesadmin/billingadmin.php
+++ pagesadmin/billingadmin.php
@@ -87,7 +87,7 @@
             <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Menu</h6>
           </li>
 
-<?php if ($_SESSION['status'] != 6) { ?>}
+<?php if ($_SESSION['status'] != 6) { ?>
           <!-- KIRIM SURAT -->
           <li class="nav-item">
             <a class="nav-link  " href="../pagesadmin/kirimadmin.php">
