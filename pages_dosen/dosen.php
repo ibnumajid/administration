dbed9d0920dca0e3f2f993f4df50ae86b0f148fa
AdnanDennis <92702339+AdnanDennis@users.noreply.github.com>
2021-11-18 20:01:19 +0700

    link nama
    untuk link nama di dosen

--- pages_dosen/dosen.php
+++ pages_dosen/dosen.php
@@ -221,9 +221,9 @@
               </div>
               <ul class="navbar-nav  justify-content-end">
                 <li class="nav-item d-flex align-items-center">
-                  <a href="" class="nav-link text-body font-weight-bold px-0">
+                  <a href="../profile.php" class="nav-link text-body font-weight-bold px-0">
                     <i class="fa fa-user me-sm-1"></i>
-                    <span class="d-sm-inline d-none"> <?php echo $_SESSION['user']?> </span>
+                    <span class="d-sm-inline d-none"><?php echo $_SESSION['user']?></span>
                   </a>
                 </li>
                 <!-- <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
