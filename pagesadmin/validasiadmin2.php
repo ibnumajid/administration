69f9265bb179079903a73a5bccc67fdbe403f339
AdnanDennis <92702339+AdnanDennis@users.noreply.github.com>
2021-11-19 11:00:10 +0700

    admin
    validasi dosen menu admin

--- pagesadmin/validasiadmin2.php
+++ pagesadmin/validasiadmin2.php
@@ -190,12 +190,12 @@
         <div class="col-12">
           <div class="card mb-4">
             <div class="card-header pb-0 p-3">
-              <!-- <div class="row">
-                <div class="col-6 d-flex align-items-center">
+              <div class="row">
+                <!--div class="col-6 d-flex align-items-center">
                   <h6 class="mb-0">Surat Masuk dari Dosen</h6>
-                </div>
+                </div-->
               </div>
-            </div> -->
+            </div>
             
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
@@ -227,14 +227,7 @@
                     <!-- tabel -->
                     <tbody>
                       <tr>
-                        <!-- file -->
                         
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <!-- <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1"> -->
-                            </div>          
-                          </div>
-                       
                         <!-- progres -->
                         <td>
                           <h6 class="mb-0 text-sm ps-3"><?php echo $data['perihal'] ?></h6>
