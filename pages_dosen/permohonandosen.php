95a8016e7d1a229caeb25b990807a303ad5557d6
arini01 <rahmadianarin@gmail.com>
2021-11-19 08:19:14 +0700

    update

--- pages_dosen/permohonandosen.php
+++ pages_dosen/permohonandosen.php
@@ -366,8 +366,12 @@
                   ?>
                       <tr>
                         <td class="text-center"><?php echo $no++ ?></td>
-                        <td><?php echo $data['perihal'] ?></td>
-                        <td class = "text-center"><?php echo $data['tanggal'] ?></td>
+                        <td>
+                          <h6 class="mb-0 text-sm ps-3"><?php echo $data['perihal'] ?></h6>
+                        </td>
+                        <td class="align-middle ps-5">
+                          <h6 class="mb-0 text-sm"><?php echo $data['tanggal'] ?></h6>
+                        </td>
 
                         <!-- status kadep -->
                         <?php if ($data['status_kadep'] == 0) { ?>
