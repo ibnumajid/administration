9110b36cc1a5123b9f78248ebd775e9b9b4ebe4b
Ibnu Majid <ibnum1976@gmail.com>
2021-11-19 09:29:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
+        <h4 class="modal-title w-100 font-weight-bold">Daftar Akun</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
