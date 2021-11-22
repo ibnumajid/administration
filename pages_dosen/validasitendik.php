2b33fdcdf3c97bb4378af849f530fab652657664
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-22 22:15:57 +0700

    membuat halaman khusus validasi tendik dan membetulkan halaman validasi dosen

--- pages_dosen/validasitendik.php
+++ pages_dosen/validasitendik.php
@@ -99,32 +99,33 @@
           </li>
           <?php } ?>
 
-        <!--Validasi Surat-->
-        <li class="nav-item">
-          <a class="nav-link  active" href="./validasidsn.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z"/>
-              </svg>
-            </div>
-            <span class="nav-link-text ms-1">Validasi Surat Dosen</span>
-          </a>
-        </li>
-
-        <!-- Validasi Surat Tendik -->
-        <?php if ($_SESSION['status'] == '5') {?>
+<!-- Validasi Surat Tendik -->
+<?php if ($_SESSION['status'] == '5') {?>
           <li class="nav-item">
-            <a class="nav-link  " href="./validasitndk.php">
+            <a class="nav-link  " href="./validasidsn.php">
               <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                 </svg>
               </div>
-              <span class="nav-link-text ms-1">Validasi Surat Tendik</span>
+              <span class="nav-link-text ms-1">Validasi Surat Dosen</span>
             </a>
           </li>
 
           <?php } ?>
+        <!--Validasi Surat-->
+        <li class="nav-item">
+          <a class="nav-link  active" href="./validasitndk.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z"/>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">Validasi Surat Tendik</span>
+          </a>
+        </li>
+
+        
 
 
         <!-- REKAP SURAT -->
@@ -315,7 +316,7 @@
                   $id = $_POST['id'];
                   include '../_database/config.php'; //panggil setiap ingin koneksi ke data
                   $nama = $_SESSION['user'];
-                  $query = mysqli_query($koneksi, "SELECT * FROM suratdosen WHERE id_no = '$id' ");
+                  $query = mysqli_query($koneksi, "SELECT * FROM surattendik WHERE id_no = '$id' ");
                   $data = mysqli_fetch_array($query)
 
                   
@@ -326,11 +327,11 @@
                               <div class="row">
                                 <div class="mb-3">
                                   <!-- nama mahasiswa -->
-                                  <label for="formFile" class="form-label">Nama Dosen</label>
-                                  <label name="nm" class="form-control" aria-label="default input example"><?php echo $data['nama_mhsw'] ?></label>
+                                  <label for="formFile" class="form-label">Nama Tendik</label>
+                                  <label name="nm" class="form-control" aria-label="default input example"><?php echo $data['nama_tdk'] ?></label>
                                   <!-- NRP mahasiswa -->
-                                  <label for="formFile" class="form-label">NIP Dosen</label>
-                                  <label name="nrp" class="form-control" aria-label="default input example"><?php echo $data['id_nrp'] ?></label>
+                                  <label for="formFile" class="form-label">NIP Tendik</label>
+                                  <label name="nrp" class="form-control" aria-label="default input example"><?php echo $data['id_nip'] ?></label>
                                   <!-- progres -->
                                   <label for="formFile" class="form-label">Perihal</label>
                                   <label name="sr" class="form-control" aria-label="default input example"><?php echo $data['perihal'] ?></label>
@@ -397,8 +398,8 @@
                      $status = $_POST['ss'];
                      
                      
-                     $query = mysqli_query($koneksi, "UPDATE suratdosen SET `catatan2`='$catatan2' WHERE id_no = '$id' ");
-                     $query2 = mysqli_query($koneksi, "UPDATE suratdosen SET `status_kadep`='$status' WHERE id_no = '$id' ");
+                     $query = mysqli_query($koneksi, "UPDATE surattendik SET `catatan2`='$catatan2' WHERE id_no = '$id' ");
+                     $query2 = mysqli_query($koneksi, "UPDATE surattendik SET `status_kadep`='$status' WHERE id_no = '$id' ");
                      if($query && $query2){
                        echo '<a href="../pages_dosen/validasisurat.php"><script> alert ("Berhasil diajukan")</script></a>';
                      }
