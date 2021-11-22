87864c02cb6a037516f58520721e9b800a081887
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-22 23:03:03 +0700

    Update dosen.php

--- pages_dosen/dosen.php
+++ pages_dosen/dosen.php
@@ -406,11 +406,11 @@
             $nama = $_SESSION['user'];
 
             $query = mysqli_query($koneksi, 'SELECT * FROM suratmahasiswa ORDER BY id_no DESC');
-
+            $no = 0;
+            $no2 = $no++;
             while ($data = mysqli_fetch_array($query)) {
               $tujuan = $data['dosen_pembimbing'];
-              $no = 0;
-              $no2 = $no++;
+              
               if (strpos($tujuan, $nama) !== false && $data['status_surat'] == 0 ) { ?>
                 <?php $id = $data['id_no'] ?>
                 <tr>
