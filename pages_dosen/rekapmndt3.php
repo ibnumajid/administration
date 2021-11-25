99b992ba3fd2fe0795b80a6d9d7398b649ed5fbc
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-25 10:10:42 +0700

    membetulkan bagian mandat kadep

--- pages_dosen/rekapmndt3.php
+++ pages_dosen/rekapmndt3.php
@@ -245,14 +245,28 @@
           </div>
           <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
-              <a href="" class="nav-link text-body font-weight-bold px-0">
+              <a href="../profile.php" href="javascript:;" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
                 <span class="d-sm-inline d-none"><?php echo $_SESSION['user'] ?></span>
               </a>
             </li>
+            <!--li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                </div>
+              </a>
+            </li-->
+            <li class="nav-item px-3 d-flex align-items-center">
+              <!--a href="javascript:;" class="nav-link text-body p-0">
+                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+              </a-->
+            </li>
             
             <li class="nav-item d-flex align-items-center">
-            <a href="../logout.php"  class="nav-link text-body p-0" >
+                <a href="../logout.php" href="javascript:;" class="nav-link text-body p-0" >
                   <i class="fas fa-sign-out-alt"></i>
                   <span class="d-sm-inline d-none">Logout </span>
                 </a>
