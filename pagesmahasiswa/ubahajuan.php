7f76ef6b6ecbbf45cdbd08fca50d639aeb6549e3
GvnP <gavin.pakpahan@gmail.cpm>
2021-11-27 23:28:16 +0700

    mengubah tampilan pada user dosen

--- pagesmahasiswa/ubahajuan.php
+++ pagesmahasiswa/ubahajuan.php
@@ -105,7 +105,7 @@ include "../_database/config.php";
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Sistem Administrasi Dosen</a></li>
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Sistem Administrasi Mahasiswa</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Permohonan Surat</li>
           </ol>
           <h5 class="font-weight-bolder mb-0">Ubah Ajuan</h5>
