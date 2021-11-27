8b71802fe35f3162526aca5ec1f879aa0f71015f
AdnanDennis <92702339+AdnanDennis@users.noreply.github.com>
2021-11-27 18:55:39 +0700

    hilanggin buttn nama

--- pages_dosen/kadep1.php
+++ pages_dosen/kadep1.php
@@ -317,12 +317,7 @@
                  <!-- no -->
                  <td class="text-center"><?php echo $no++ ?></td>
                  <!-- nama -->
-                 <form action="./validasimhs.php" method="post">
-                 <input name="id" value=<?php echo $data['id_no'] ?> type="hidden">
-                 <td style = "height:20px">
-                 <h6 style = "height:35px" class="text-sm-left ps-1 "><button class="btn btn-light"><?php echo $data['nama_mhsw'] ?></button></h6>
-                 </td>
-                 </form>
+                 <td><h6 class="text-sm-left ps-1 "><?php echo $data['nama_mhsw'] ?></h6></td>
                  <!-- status -->
                  <td> <h6 class="text-left ps-1">Mahasiswa</h6></td>
                  <!-- perihal -->
@@ -360,13 +355,8 @@
                <tr>
                  <!-- no -->
                  <td class="text-center"><?php echo $no++ ?></td>
-                 <!-- nama -->
-                 <form action="./validasidosen.php" method="post">
-                 <input name="id" value=<?php echo $data['id_no'] ?> type="hidden">
-                 <td style = "height:20px">
-                   <h6 style = "height:20px" class="text-sm-left"><button style ="width:250px" class="btn btn-light btn-sm"><?php echo $data['nama_dsn'] ?></button></h6>
-                 </td>
-               </form>
+                 <!-- nama --> 
+                 <td><h6 style = "height:20px" class="text-sm-left"><?php echo $data['nama_dsn'] ?></h6></td>
                  <!-- status -->
                  <td> <h6 class="text-left ps-1">Dosen</h6></td>
                  <!-- perihal -->
@@ -405,12 +395,7 @@
                  <!-- no -->
                  <td class="text-center"><?php echo $no++ ?></td>
                  <!-- nama -->
-                 <form action="./validasitendik.php" method="post">
-                 <input name="id" value=<?php echo $data['id_no'] ?> type="hidden">
-                 <td style = "height:20px">
-                   <h6 style = "height:20px" class="text-sm-left"><button style ="width:250px" class="btn btn-light btn-sm"><?php echo $data['nama_tdk'] ?></button></h6>
-                 </td>
-               </form>
+                 <td><h6 style = "height:20px" class="text-sm-left"><?php echo $data['nama_tdk'] ?></h6></td>
                  <!-- status -->
                  <td> <h6 class="text-left ps-1">Tendik</h6></td>
                  <!-- perihal -->
