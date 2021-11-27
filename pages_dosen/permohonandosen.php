57cbd0ac105c1931899358160bd1cd7da6707139
arini01 <rahmadianarin@gmail.com>
2021-11-27 16:34:10 +0700

    font dan nama menu

--- pages_dosen/permohonandosen.php
+++ pages_dosen/permohonandosen.php
@@ -181,7 +181,7 @@
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Sistem Administrasi Dosen</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Permohonan Surat</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Permohonan Surat</h6>
+          <h5 class="font-weight-bolder mb-0">Permohonan Surat</h5>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
