c3116e643c36c7af15bfb1269c3d7df19e3dfbc8
arini01 <rahmadianarin@gmail.com>
2021-11-27 15:54:50 +0700

    bagian kiri atas

--- pagesmahasiswa/mahasiswa.php
+++ pagesmahasiswa/mahasiswa.php
@@ -72,7 +72,7 @@ session_start();
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="">
         
-        <span class="ms-1 font-weight-bold">Sistem Administrasi mahasiswa</span>
+        <span class="ms-0 font-weight-bold">Sistem Administrasi Mahasiswa</span>
       </a>
     </div>
 
--- pagesmahasiswa/rekapmhs.php
+++ pagesmahasiswa/rekapmhs.php
@@ -33,7 +33,7 @@ if ($_SESSION['user'] == '') {
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="">
         
-        <span class="ms-1 font-weight-bold">Sistem Administrasi Mahasiswa</span>
+        <span class="ms-0 font-weight-bold">Sistem Administrasi Mahasiswa</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
