4103ba052f92dea4ccc1c618924d141685c1d7b9
arini01 <rahmadianarin@gmail.com>
2021-11-30 09:18:37 +0700

    Update kirimkadep.php

--- pages_dosen/kirimkadep.php
+++ pages_dosen/kirimkadep.php
@@ -203,7 +203,7 @@ session_start();
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Sistem Administrasi Kadep</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Pengajuan Mandat</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Pengajuan Mandat</h6>
+          <h5 class="font-weight-bolder mb-0">Pengajuan Mandat</h5>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
