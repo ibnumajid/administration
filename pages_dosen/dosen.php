372e9ec1167af9b6bdb6ffe518c4ccbb7b6b467f
arini01 <rahmadianarin@gmail.com>
2021-12-01 15:06:32 +0700

    panggilan untuk membaca dokumen

--- pages_dosen/dosen.php
+++ pages_dosen/dosen.php
@@ -457,7 +457,9 @@
                             { $no++ 
                             ?>
                         <blockquote class="blockquote ps-0">
-                          <p class="ps-3 text-dark"><?=$data['perihal']?></p>
+                        <a href="../pagesadmin/<?php echo $data['dokumen'] ?>" target="_blank">
+                          <p class="ps-3 text-dark" name="fl" id="edit<?php echo $data['id_no'] ?>"><?=$data['perihal']?></p>
+                        </a>
                         </blockquote>
                         <figcaption class="blockquote-footer ps-3 text-dark">
                           <?=$data['keterangan']?>
