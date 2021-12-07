39014d2265c4f01951a12b3b384a10c6e2fb0f4e
arini01 <rahmadianarin@gmail.com>
2021-12-07 08:55:14 +0700

    rekapmandat

--- pages_dosen/rekapdsn.php
+++ pages_dosen/rekapdsn.php
@@ -190,7 +190,7 @@
           <?php } ?>
 
           <!-- REKAP SURAT -->
-          <?php if($_SESSION['status'] !== '5'){ ?>
+          <?php if($_SESSION['status2'] !== '5'){ ?>
           <li class="nav-item"> 
             <a class="nav-link  " href="./rekapmndt.php">
               <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
