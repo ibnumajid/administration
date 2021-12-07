5963e6550fa09fc140058350ef8bf5bfa7445135
AdnanDennis <92702339+AdnanDennis@users.noreply.github.com>
2021-12-07 15:28:55 +0700

    perubahan diajukan surat
    mahasiswa

--- pagesmahasiswa/ajukansurat.php
+++ pagesmahasiswa/ajukansurat.php
@@ -49,7 +49,7 @@ else if ($tipe != $ekstensi && $tipe != NULL)
 else if (move_uploaded_file($_FILES['fl']['tmp_name'], $url)) 
 {
   if ($perihal == "Surat Magang" || $perihal == "Surat Proyek Akhir" || $perihal == "Surat PBL (Project Based Learning)") { 
-    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$tingkat', '$dosen1', '0', '$dosen2', '0', '$dosen_tkk', '9', '0', '0', '$tgl_h1', '$tgl_h2', '0', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate())");
+    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$dosen1', '0', '$dosen2', '0', '$dosen_tkk', '9', '0', '0', '$tgl_h1', '$tgl_h2', '0', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate(), '$tingkat')");
     if($query)
     {
     ?><script><?php $_SESSION['sukses'] = true;?></script> 
@@ -62,7 +62,7 @@ else if (move_uploaded_file($_FILES['fl']['tmp_name'], $url))
     }
   }
   else if ($perihal == "Surat Cuti" || $perihal == "Surat Mengundurkan Diri") {
-    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$tingkat', '$dosen1', '2', '$dosen2', '2', '$dosen_tkk', '2', '0', '0', '$tgl_h1', '$tgl_h2', '2', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate())");
+    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$dosen1', '2', '$dosen2', '2', '$dosen_tkk', '2', '0', '0', '$tgl_h1', '$tgl_h2', '2', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate(), '$tingkat')");
     if($query)
     {
     ?><script><?php $_SESSION['sukses'] = true;?></script> 
@@ -75,7 +75,7 @@ else if (move_uploaded_file($_FILES['fl']['tmp_name'], $url))
     }
   }
   else if ($perihal == "Surat Pengajuan Beasiswa" || $perihal == "Surat Keringanan UKT" || $perihal == "Surat Permohonan Lomba" || $perihal == "Surat Pengajuan Kegiatan HIMA" ) {
-    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$tingkat', '$dosen1', '9', '$dosen2', '9', '$dosen_tkk', '0', '0', '0', '$tgl_h1', '$tgl_h2', '1', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate())");
+    $query = mysqli_query($koneksi,"insert into suratmahasiswa values('', '3', '$nama_mhsw','$id_nrp','$perihal','$keterangan','$judul_ta', '$dosen1', '9', '$dosen2', '9', '$dosen_tkk', '0', '0', '0', '$tgl_h1', '$tgl_h2', '1', '$url', '', '', '',  '', '$tipe', '$ukuran', sysdate(), '$tingkat')");
     if($query) 
     {
     ?><script><?php $_SESSION['sukses'] = true;?></script> 
